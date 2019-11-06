8f0973de870a32fbdcf690f6b255c00a2f165914
Emerick NEEL <emerickneel@gmail.com>
2019-11-06 10:31:57 +0100

    TP2

--- main.js
+++ main.js
@@ -1,41 +1,12 @@
-const Pizza = require('./pizza.js')
 const readline = require('readline')
+const Pizza = require('./pizza');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-let tabPizza = [];
-let inputType = "Menu";
-
-function printMenu() {
-    console.log('***** Pizzeria Administration *****')
-    console.log('1. Lister les pizzas')
-    console.log('2. Ajouter une nouvelle pizza ')
-    console.log('3. Mettre à jour une pizza')
-    console.log('4. Supprimer une pizza ')
-    console.log('99. Sortir ')
-}
-
-function pizzaList(){
-    tabPizza.forEach(unePizza => {
-        console.log(unePizza.id + ' ' + unePizza.libelle + ' : ' + unePizza.prix +  ' €');
-    });
-    printMenu()
-}
-
-function input_user(inputVal){
-    if(inputType === "Menu"){
-        switch(inputVal){
-            case "1" :
-                pizzaList()
-                break;
-        }
-    }
-}
-
-function createPizza(){
+function generateMenu() {
     pizzas = [
         new Pizza(0, "PEP", "Pépéroni", 12.50),
         new Pizza(1, "MAR", "Margharita", 14.00),
@@ -46,12 +17,121 @@ function createPizza(){
         new Pizza(6, "ORI", "L'orientale", 13.50),
         new Pizza(7, "IND", "L'indienne", 14.00),
     ];
-
-    tabPizza = pizzas;
 }
 
-//Génération de la liste des pizzas
-createPizza();
+function findPizza(code) {
+    let pizza = new Pizza();
+    for (this.i = 0; this.i < this.pizzas.length; this.i++) {
+        if (this.pizzas[this.i].code == code) {
+            pizza = this.pizzas[this.i];
+        }
+    }
+    return pizza;
+}
 
+
+function printMenu() {
+    console.log('***** Pizzeria Administration *****')
+    console.log('1. Lister les pizzas')
+    console.log('2. Ajouter une nouvelle pizza ')
+    console.log('3. Mettre à jour une pizza')
+    console.log('4. Supprimer une pizza ')
+    console.log('99. Sortir ')
+}
+
+function pizzaList(bMenu = false) {
+    pizzas.forEach(pizza => {
+        console.log(pizza.code + " -> " + pizza.libelle + " (" + pizza.prix + " €)");
+    });
+    if(bMenu == true){
+        printMenu();
+    }    
+}
+
+function pizzaAdd(bMenu = false) {
+
+    let pizza = new Pizza();
+
+    //Récupère l'ID de la pizza
+    pizzas.id = pizzas.length;
+    rl.question('Veuillez choisir le code : ', (pizzaCode) => {
+        pizza.code = pizzaCode;
+        rl.question('Veuillez choisir le libelle : ', (pizzaLibelle) => {
+            pizza.libelle = pizzaLibelle;
+            rl.question('Veuillez choisir le prix : ', (pizzaPrix) => {
+                pizza.prix = Number(pizzaPrix);
+                pizzas.push(pizza);
+                if(bMenu == true){
+                    printMenu();
+                } 
+            });
+        });
+    });    
+}
+
+function pizzaUpdate(bMenu = false, pizzaCode) {
+    let pizza = findPizza(pizzaCode);
+    rl.question('Veuillez choisir le nouveau code : ', (pizzaCode) => {
+        pizza.code = pizzaCode;
+        rl.question('Veuillez choisir le nouveau libelle : ', (pizzaLibelle) => {
+            pizza.libelle = pizzaLibelle;
+            rl.question('Veuillez choisir le nouveau prix : ', (pizzaPrix) => {
+                pizza.prix = Number(pizzaPrix);
+                pizzas[pizza.id] = pizza;
+                if(bMenu == true){
+                    printMenu();
+                }
+            });
+        });
+    });
+}
+
+function pizzaDelete(bMenu = false, pizzaCode) {
+    let pizza = findPizza(pizzaCode);
+    pizzas.splice(pizza.id,1);
+    if(bMenu == true){
+        printMenu();
+    }
+}
+
+generateMenu()
 printMenu()
-rl.on('line', input_user)
+rl.prompt();
+
+rl.on('line', (line) => {
+    switch (line.trim()) {
+        case '1':
+            console.log('Liste des pizzas');
+            pizzaList(true);
+            break;
+        case '2':
+            console.log("Ajout d'une pizza");
+            pizzaAdd(true);
+            break;
+        case '3':
+            console.log("Mise à jour d'une pizza");
+            pizzaList(false);
+            rl.question('Veuillez choisir le code ', (pizzaCode) => {
+                pizzaUpdate(true,pizzaCode);
+            })
+            break;
+        case '4':
+            console.log("Suppression d'une pizza");
+            pizzaList(false);
+            rl.question('Veuillez choisir le code ', (pizzaCode) => {
+                pizzaDelete(true,pizzaCode);
+            })
+            break;
+        case '99':
+            console.log("Fin de l'application");
+            process.exit(0);
+            break;
+        default:
+            pizzaList();
+            break;
+    }
+    rl.prompt();
+}).on('close', () => {
+    console.log("Fin de l'application");
+    process.exit(0);
+});
